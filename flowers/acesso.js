382320a69c1434b9df2f25b7210da0d9d43b8f8b
futuraDEV <147119862+raraii2@users.noreply.github.com>
2023-11-07 22:23:15 -0300

    Add files via upload

---
+++ flowers/acesso.js
@@ -0,0 +1,47 @@
+const btn = document.querySelector("#btn")
+
+const usuario = document.querySelector(".usuario").value;
+
+const senha = document.querySelector(".senha").value;
+
+
+function send(){
+
+const usuario = document.querySelector(".usuario").value;
+
+const senha = document.querySelector(".senha").value;
+
+const preencha = document.querySelector("#preencha")
+
+const sucesso = document.querySelector("#sucesso")
+
+
+if(usuario.length <1 || senha.length <1){
+    
+    preencha.style.display = ('inline')
+
+    setTimeout(() => {
+
+        preencha.style.display = ('none')
+    
+        }, 1000);
+
+    return false;
+}
+
+if(usuario == "Sua Namorada" && senha == "TeAmoMinhaGarota"){
+
+    sucesso.style.display = ('inline')
+    setTimeout(() => {
+
+    location.href = "inicio.html";
+
+    }, 500);
+    
+} else {
+    negado.style.display = "inline";
+    setTimeout(() => {
+        location.href = "erro.html";
+    }, 500);
+}
+}
